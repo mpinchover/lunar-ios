c0c9034980461855a915377d2b6dd88203a3d01c
mpinchover <mpinchover@gmail.com>
2026-04-26 11:54:46 -0700

    Initial Commit

---
+++ lunar/lunarApp.swift
@@ -0,0 +1,17 @@
+//
+//  lunarApp.swift
+//  lunar
+//
+//  Created by Matt Pinchover on 4/26/26.
+//
+
+import SwiftUI
+
+@main
+struct lunarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lunarTests/lunarTests.swift
@@ -0,0 +1,17 @@
+//
+//  lunarTests.swift
+//  lunarTests
+//
+//  Created by Matt Pinchover on 4/26/26.
+//
+
+import Testing
+@testable import lunar
+
+struct lunarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
